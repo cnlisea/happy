928e4ef1921b96b37f0df069ffe139ce49388f16
lisea <cnlisea@126.com>
2022-04-09 16:03:07 +0800

    fix delay proxy

--- proxy/delay.go
+++ proxy/delay.go
@@ -4,4 +4,6 @@ import "time"
 
 type Delay interface {
 	Add(delayTime time.Duration, f func(ts int64, args interface{}), arg interface{})
+	Range(f func(ts int64, args interface{}) bool)
+	Del(f func(ts int64, args interface{}) bool)
 }

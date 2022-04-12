3e441b620ff7e3a5592c2fbb746460dc250b45c9
lisea <cnlisea@126.com>
2022-04-13 00:45:29 +0800

    add heartbeat existed check

--- heartbeat.go
+++ heartbeat.go
@@ -11,6 +11,10 @@ func (h *Happy) Heartbeat(interval time.Duration) error {
 		return errors.New("interval invalid")
 	}
 
+	if h.heartbeat != nil {
+		return errors.New("heartbeat already existed")
+	}
+
 	h.heartbeat = heartbeat.New(h.delay, interval, func() {
 		// 超时解散
 		h.Finish(false)

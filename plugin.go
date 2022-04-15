5017c8e23ed12f9ab437b597fde1d9de6cfeb855
lisea <cnlisea@126.com>
2022-04-15 17:05:20 +0800

    add location distance plugin

--- plugin.go
+++ plugin.go
@@ -1,11 +1,14 @@
 package happy
 
+import "github.com/cnlisea/happy/pmgr/player"
+
 type Plugin struct {
 	PlayerExitDisband   func(userKey interface{}, ownerUserKey interface{}, extend map[string]interface{}) bool
 	DisbandMinAgreeNum  func(num int) int
 	DisbandDeadlinePass func(extend map[string]interface{}) bool
 	QuickMinAgreeNum    func(num int) int
 	QuickDeadlinePass   func(extend map[string]interface{}) bool
+	LocationDistance    func(origin *player.Location, des ...*player.Location) ([]int, error)
 }
 
 func (h *_Happy) Plugin(p *Plugin) {

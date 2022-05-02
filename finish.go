c6fc73206e26feadb6df7850613d909e9c947c3d
lisea <cnlisea@126.com>
2022-05-02 15:02:59 +0800

    fix finish

--- finish.go
+++ finish.go
@@ -2,11 +2,8 @@ package happy
 
 func (h *_Happy) Finish(disband bool) {
 	h.game.Finish(disband)
-	if h.event != nil && h.event.Finish != nil {
-		h.event.Finish(h.curRound, h.maxRound, h.pMgr, disband, h.extend)
-	}
 
-	if h.costMode == CostModeFinish {
+	if h.curRound > 0 && h.costMode == CostModeFinish {
 		if h.event != nil && h.event.Cost != nil {
 			h.event.Cost(h.costMode, false, h.pMgr, h.extend)
 		}
@@ -14,6 +11,10 @@ func (h *_Happy) Finish(disband bool) {
 
 	if h.curRound == 0 && h.costMode == CostModeJoin && h.event != nil && h.event.Cost != nil {
 		h.event.Cost(h.costMode, true, h.pMgr, h.extend)
+	}
+
+	if h.event != nil && h.event.Finish != nil {
+		h.event.Finish(h.curRound, h.maxRound, h.pMgr, disband, h.extend)
 	}
 
 	if h.msgChan != nil {
